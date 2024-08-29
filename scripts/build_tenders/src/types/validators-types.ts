afca5d145e6aff6c81b5e87cea30a24167223e70
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-08-29 17:40:07 +0200

    feat(Pn-12263): add types

---
+++ scripts/build_tenders/src/types/validators-types.ts
@@ -0,0 +1,46 @@
+export type Validator = (value: string) => boolean | string | number;
+export type NumberValidator = (value: string) => number;
+export type StringValidator = (value: string) => string;
+export type BooleanValidator = (value: string) => boolean;
+
+export type TenderValidators = {
+  activationDate: StringValidator;
+  tenderName: StringValidator;
+  vat: NumberValidator;
+  nonDeductibleVat: NumberValidator;
+  fee: NumberValidator;
+  pagePrice: NumberValidator;
+  basePriceAR: NumberValidator;
+  basePriceRS: NumberValidator;
+  basePrice890: NumberValidator;
+};
+
+export type TenderCostsValidators = {
+  product: StringValidator;
+  lot: StringValidator;
+  zone: StringValidator;
+  deliveryDriverName: StringValidator;
+  deliveryDriverId: StringValidator;
+  dematerializationCost: NumberValidator;
+  range: NumberValidator;
+};
+
+export type DeliveryDriverValidators = {
+  deliveryDriverId: StringValidator;
+  taxId: StringValidator;
+  businessName: StringValidator;
+  fiscalCode: StringValidator;
+  pec: StringValidator;
+  phoneNumber: StringValidator;
+  registeredOffice: StringValidator;
+};
+
+export type GeokeyValidators = {
+  activationDate: StringValidator;
+  geokey: StringValidator;
+  product: StringValidator;
+  lot: StringValidator;
+  zone: StringValidator;
+  coverFlag: BooleanValidator;
+  dismissed: BooleanValidator;
+};
