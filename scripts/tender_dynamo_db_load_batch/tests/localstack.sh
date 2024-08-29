c2864ea32b27d02960fcff0818779cce5ffea789
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2024-08-29 17:19:40 +0200

    PN-12264 add real tables schemas

--- scripts/tender_dynamo_db_load_batch/tests/localstack.sh
+++ scripts/tender_dynamo_db_load_batch/tests/localstack.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -e
+#! /bin/bash
 
 AWS_URL_ENDPOINT=http://localhost:4566
 AWS_S3_BUCKET=tender-bucket
@@ -13,15 +13,60 @@ aws s3 mb s3://${AWS_S3_BUCKET} --endpoint-url ${AWS_URL_ENDPOINT}
 echo "Uploading file to S3: ${ZIP_FILE}"
 aws s3 cp ${ZIP_FILE} s3://${AWS_S3_BUCKET} --endpoint-url ${AWS_URL_ENDPOINT}
 
-### Create DynamoDB Tables
+### PaperChannelCost Create DynamoDB Tables
+echo "Creating PaperChannelCost table"
 aws --profile default --region eu-south-1 --endpoint-url=${AWS_URL_ENDPOINT} \
     dynamodb create-table \
-    --table-name pn-PaperCost \
+    --table-name pn-PaperChannelCost \
     --attribute-definitions \
-      AttributeName=driverCode,AttributeType=S \
-      AttributeName=uuidCode,AttributeType=S \
+      AttributeName=tenderId,AttributeType=S \
+      AttributeName=productLotZone,AttributeType=S \
     --key-schema \
-      AttributeName=driverCode,KeyType=HASH \
-      AttributeName=uuidCode,KeyType=RANGE \
+      AttributeName=tenderId,KeyType=HASH \
+      AttributeName=productLotZone,KeyType=RANGE \
     --provisioned-throughput \
-      ReadCapacityUnits=5,WriteCapacityUnits=5
+      ReadCapacityUnits=5,WriteCapacityUnits=5 \
+      || true
+
+### PaperChannelGeokey Create DynamoDB Tables
+echo "Creating PaperChannelGeokey table"
+aws --profile default --region eu-south-1 --endpoint-url=${AWS_URL_ENDPOINT} \
+    dynamodb create-table \
+    --table-name pn-PaperChannelGeokey \
+    --attribute-definitions \
+      AttributeName=tenderProductGeokey,AttributeType=S \
+      AttributeName=activationDate,AttributeType=S \
+    --key-schema \
+      AttributeName=tenderProductGeokey,KeyType=HASH \
+      AttributeName=activationDate,KeyType=RANGE \
+    --provisioned-throughput \
+      ReadCapacityUnits=5,WriteCapacityUnits=5 \
+      || true
+
+### PaperChannelTender Create DynamoDB Tables
+echo "Creating PaperChannelTender table"
+aws --profile default --region eu-south-1 --endpoint-url=${AWS_URL_ENDPOINT} \
+    dynamodb create-table \
+    --table-name pn-PaperChannelTender \
+    --attribute-definitions \
+      AttributeName=tenderId,AttributeType=S \
+      AttributeName=activationDate,AttributeType=S \
+    --key-schema \
+      AttributeName=tenderId,KeyType=HASH \
+      AttributeName=activationDate,KeyType=RANGE \
+    --provisioned-throughput \
+      ReadCapacityUnits=5,WriteCapacityUnits=5 \
+      || true
+
+### Create PaperChannelDeliveryDriver DynamoDB Tables
+echo "Creating PaperChannelDeliveryDriver table"
+aws --profile default --region eu-south-1 --endpoint-url=${AWS_URL_ENDPOINT} \
+    dynamodb create-table \
+    --table-name pn-PaperChannelDeliveryDriver \
+    --attribute-definitions \
+      AttributeName=deliveryDriverId,AttributeType=S \
+    --key-schema \
+      AttributeName=deliveryDriverId,KeyType=HASH \
+    --provisioned-throughput \
+      ReadCapacityUnits=5,WriteCapacityUnits=5 \
+      || true
