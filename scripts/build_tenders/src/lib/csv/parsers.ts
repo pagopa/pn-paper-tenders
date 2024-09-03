04f49f6b50ba624b42ee24dcbf232d50c93618cf
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-09-03 14:25:57 +0200

    feat((PN-12263)): add new parser function for Geokey CSV files with support for an additional file version check

--- scripts/build_tenders/src/lib/csv/parsers.ts
+++ scripts/build_tenders/src/lib/csv/parsers.ts
@@ -4,7 +4,10 @@ import {
   TenderCostsValidators,
   TenderValidators,
 } from '../../types/validators-types';
-import { rangeColumnPattern } from '../../utils/regex';
+import {
+  geokeyFileVersionPattern,
+  rangeColumnPattern,
+} from '../../utils/regex';
 import {
   booleanValidator,
   dateTimeUtcStringValidator,
@@ -18,7 +21,7 @@ import {
 import { parseColCsvFun } from './reader';
 
 /**
- * A map of validator functions for validating columns in the Tender 
+ * A map of validator functions for validating columns in the Tender
  * CSV files.
  */
 const tenderValidatorsMap: TenderValidators = {
@@ -34,7 +37,7 @@ const tenderValidatorsMap: TenderValidators = {
 };
 
 /**
- * A map of validator functions for validating columns in the TenderCosts 
+ * A map of validator functions for validating columns in the TenderCosts
  * CSV files.
  */
 const tenderCostsValidatorsMap: TenderCostsValidators = {
@@ -48,7 +51,7 @@ const tenderCostsValidatorsMap: TenderCostsValidators = {
 };
 
 /**
- * A map of validator functions for validating columns in the Geokey 
+ * A map of validator functions for validating columns in the Geokey
  * CSV files.
  */
 const geokeyValidatorsMap: GeokeyValidators = {
@@ -62,7 +65,7 @@ const geokeyValidatorsMap: GeokeyValidators = {
 };
 
 /**
- * A map of validator functions for validating columns in the DeliveryDriver 
+ * A map of validator functions for validating columns in the DeliveryDriver
  * CSV files.
  */
 const deliveryDriverValidatorsMap: DeliveryDriverValidators = {
@@ -77,7 +80,7 @@ const deliveryDriverValidatorsMap: DeliveryDriverValidators = {
 
 /**
  * Throws an error if the column is invalid for the given validators map.
- * 
+ *
  * @param validators - The map of validators for the given CSV type.
  * @param key - The column key to validate.
  * @param filePath - The path to the CSV file being validated.
@@ -95,19 +98,19 @@ const throwInvalidColumnError = (
 
 /**
  * Parses a column from a Tender CSV file.
- * 
+ *
  * @param value - The value to be validated and parsed.
  * @param column - The column name of the value.
  * @param filePath - The path to the CSV file being parsed.
  * @returns The parsed and validated value.
- * @throws Will throw an error if the column is invalid or the value 
+ * @throws Will throw an error if the column is invalid or the value
  * fails validation.
  */
 export const parseTenderColumn: parseColCsvFun = (
   value: string,
   column: string,
   filePath: string
-): unknown => {
+) => {
   throwInvalidColumnError(tenderValidatorsMap, column, filePath);
   const key = column as keyof TenderValidators;
   return tenderValidatorsMap[key](value);
@@ -115,20 +118,31 @@ export const parseTenderColumn: parseColCsvFun = (
 
 /**
  * Parses a column from a TenderCosts CSV file.
- * 
+ *
  * @param value - The value to be validated and parsed.
  * @param column - The column name of the value.
  * @param filePath - The path to the CSV file being parsed.
  * @returns The parsed and validated value.
- * @throws Will throw an error if the column is invalid or the value 
+ * @throws Will throw an error if the column is invalid or the value
  * fails validation.
  */
 export const parseTenderCostsColumn: parseColCsvFun = (
   value: string,
   column: string,
   filePath: string
-): unknown => {
+) => {
+  // range key validation
   if (rangeColumnPattern.test(column)) {
+    const match = column.match(rangeColumnPattern);
+
+    const minWeight = parseInt(match![1]!, 10);
+    const maxWeight = parseInt(match![2]!, 10);
+
+    if (minWeight >= maxWeight) {
+      throw new Error(
+        `Invalid column ${column} in file ${filePath}: minWeight must be < maxWeight`
+      );
+    }
     return tenderCostsValidatorsMap.range(value);
   }
   throwInvalidColumnError(tenderCostsValidatorsMap, column, filePath);
@@ -138,39 +152,47 @@ export const parseTenderCostsColumn: parseColCsvFun = (
 
 /**
  * Parses a column from a Geokey CSV file.
- * 
+ *
  * @param value - The value to be validated and parsed.
  * @param column - The column name of the value.
  * @param filePath - The path to the CSV file being parsed.
  * @returns The parsed and validated value.
- * @throws Will throw an error if the column is invalid or the value 
+ * @throws Will throw an error if the column is invalid or the value
  * fails validation.
  */
 export const parseGeokeyColumn: parseColCsvFun = (
   value: string,
   column: string,
   filePath: string
-): unknown => {
+) => {
   throwInvalidColumnError(geokeyValidatorsMap, column, filePath);
+  const match = filePath.match(geokeyFileVersionPattern);
+  if(!match) {
+    throw new Error(`Invalid geokey file name ${filePath}`);
+  }
+  // Geokey_v1
+  if (match[1] === '1' && column === 'activationDate' && value.trim() === '') {
+    return '';
+  }
   const key = column as keyof GeokeyValidators;
   return geokeyValidatorsMap[key](value);
 };
 
 /**
  * Parses a column from a DeliveryDriver CSV file.
- * 
+ *
  * @param value - The value to be validated and parsed.
  * @param column - The column name of the value.
  * @param filePath - The path to the CSV file being parsed.
  * @returns The parsed and validated value.
- * @throws Will throw an error if the column is invalid or the value 
+ * @throws Will throw an error if the column is invalid or the value
  * fails validation.
  */
 export const parseDeliveryDriverColumn: parseColCsvFun = (
   value: string,
   column: string,
   filePath: string
-): unknown => {
+) => {
   throwInvalidColumnError(deliveryDriverValidatorsMap, column, filePath);
   const key = column as keyof DeliveryDriverValidators;
   return deliveryDriverValidatorsMap[key](value);
